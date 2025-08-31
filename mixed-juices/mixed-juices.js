b493d8dbea7448b869ccb9a6e5ba57934f237beb
Olivier Odiet <olivier.odiet@gmail.com>
2025-08-31 16:03:25 +0100

    Mixed Juices

---
+++ mixed-juices/mixed-juices.js
@@ -0,0 +1,79 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines how long it takes to prepare a certain juice.
+ *
+ * @param {string} name
+ * @returns {number} time in minutes
+ */
+export function timeToMixJuice(name) {
+  switch (name) {
+    case 'Pure Strawberry Joy':
+      return 0.5;
+    case 'Energizer':
+      return 1.5;
+    case 'Green Garden':
+      return 1.5;
+    case 'Tropical Island':
+      return 3;
+    case 'All or Nothing':
+      return 5;
+    default:
+      return 2.5;
+  }
+}
+
+/**
+ * Calculates the number of limes that need to be cut
+ * to reach a certain supply.
+ *
+ * @param {number} wedgesNeeded
+ * @param {string[]} limes
+ * @returns {number} number of limes cut
+ */
+export function limesToCut(wedgesNeeded, limes) {
+  const NUMBER_OF_WEDGE_PER_SMALL = 6;
+  const NUMBER_OF_WEDGE_PER_MEDIUM = 8;
+  const NUMBER_OF_WEDGE_PER_LARGE = 10;
+  let totalNumberOfWedges = 0;
+  let limesToCut = 0;
+
+  while (totalNumberOfWedges < wedgesNeeded && limesToCut < limes.length) {
+    switch (limes[limesToCut]) {
+      case 'small':
+        totalNumberOfWedges += NUMBER_OF_WEDGE_PER_SMALL;
+        break;
+      case 'medium':
+        totalNumberOfWedges += NUMBER_OF_WEDGE_PER_MEDIUM;
+        break;
+      case 'large':
+        totalNumberOfWedges += NUMBER_OF_WEDGE_PER_LARGE;
+        break;
+    }
+    limesToCut++;
+  }
+
+  return limesToCut;
+}
+
+/**
+ * Determines which juices still need to be prepared after the end of the shift.
+ *
+ * @param {number} timeLeft
+ * @param {string[]} orders
+ * @returns {string[]} remaining orders after the time is up
+ */
+export function remainingOrders(timeLeft, orders) {
+  let orderNumber = 0;
+
+  while (timeLeft > 0) {
+    timeLeft -= timeToMixJuice(orders[orderNumber]);
+    orders.shift();
+  }
+
+  return orders;
+}
