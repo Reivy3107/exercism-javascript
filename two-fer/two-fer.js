09f9e6204deafe554451d1d862e82c6f6d83c63a
Olivier Odiet <olivier.odiet@gmail.com>
2025-08-31 16:20:37 +0100

    Two Fer

---
+++ two-fer/two-fer.js
@@ -0,0 +1,8 @@
+// @ts-check
+
+/**
+ * Return what to say when giving an extra cookie
+ * @param {string} [name='you'] - The name of the customer
+ * @returns {string} - The string to say
+ */
+export const twoFer = (name = 'you') => `One for ${name}, one for me.`;
